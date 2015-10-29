1b6607cd6d67358ad77a073dd0fb2ff3c1e00c17
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-29 13:10:06 -0700

    Set 1, Exercise 2

--- set1/exercise2.js
+++ set1/exercise2.js
@@ -1,5 +1,16 @@
 module.exports = fixedXOR;
 
+var HEX_CHARS = '0123456789abcdef';
+
 function fixedXOR(str1, str2) {
-  
+  var length = Math.min(str1.length, str2.length);
+      result = '';
+
+  for (var i = 0; i < length; ++i) {
+    var a = parseInt(str1[i], 16),
+        b = parseInt(str2[i], 16);
+    result += HEX_CHARS[a ^ b];
+  }
+
+  return result;
 }
