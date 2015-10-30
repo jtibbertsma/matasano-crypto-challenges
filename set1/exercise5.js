6964ea2aca211035a56d51918075f6af6f14bcc6
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-29 18:47:58 -0700

    Set 1, Exercise 5

--- set1/exercise5.js
+++ set1/exercise5.js
@@ -1,6 +1,15 @@
 module.exports = repeatXOREncode;
 
-// Assumes only one line of text
 function repeatXOREncode(text, key) {
-  
+  var encoded = '';
+
+  for (var i = 0; i < text.length; i++) {
+    var v1 = text[i].charCodeAt(0),
+        v2 = key[i % key.length].charCodeAt(0);
+
+    var hex = Number(v1 ^ v2).toString(16);
+    encoded += hex.length === 1 ? '0' + hex : hex;
+  }
+
+  return encoded;
 }
--- spec/XORencoderSpec.js
+++ spec/XORencoderSpec.js
@@ -2,15 +2,8 @@ describe('repeating-key XOR Encoder', function () {
   var encode = require('../set1/exercise5');
 
   it('encodes the first example', function () {
-    var decoded = "Burning 'em, if you ain't quick and nimble",
-        encoded = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272";
-
-    expect(encode(decoded, "ICE")).toEqual(encoded);
-  });
-
-  it('encodes the second example', function () {
-    var decoded = "I go crazy when I hear a cymbal",
-        encoded = "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+    var decoded = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+        encoded = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
     expect(encode(decoded, "ICE")).toEqual(encoded);
   });
