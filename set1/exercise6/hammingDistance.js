e93950793417d7ebfeb77afe7150efe6dca31d5a
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-30 14:13:25 -0700

    Implement function to calculate Hamming distance between two strings

--- set1/exercise6/hammingDistance.js
+++ set1/exercise6/hammingDistance.js
@@ -2,5 +2,25 @@ module.exports = hammingDistance;
 
 // arguments are byte arrays
 function hammingDistance(data1, data2) {
-  
+  if (data1.length !== data2.length)
+    throw "Error: strings should be the same length";
+
+  var distance = 0;
+
+  for (var i = 0; i < data1.length; ++i) {
+    var a = data1[i], b = data2[i];
+
+    distance += byteDistance(a, b);
+  }
+
+  return distance;
+}
+
+function byteDistance(a, b) {
+  var distance = 0;
+  for (var i = 0; i < 8; i++) {
+    if ((a & (1 << i)) !== (b & (1 << i)))
+      distance += 1;
+  }
+  return distance;
 }
