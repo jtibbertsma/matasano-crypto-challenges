ba9939015c566e0583fd59cb6edc24cc6dc6cac9
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-11-07 12:23:37 -0800

    Preface require strings with ./

--- run_s1e6.js
+++ run_s1e6.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-var metric = require('set1/exercise6/metric');
-var decompose = require('set1/exercise6/decompose');
-var bestKeyLength = require('set1/exercise6/bestKeyLength');
-var base64 = require('set1/exercise6/base64decode');
+var metric = require('./set1/exercise6/metric');
+var decompose = require('./set1/exercise6/decompose');
+var bestKeyLength = require('./set1/exercise6/bestKeyLength');
+var base64 = require('./set1/exercise6/base64decode');
 
 function main(data) {
   var keyLengths = bestKeyLength(data),
