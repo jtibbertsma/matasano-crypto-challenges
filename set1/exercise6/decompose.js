2e64cbf5b2c8bc3f1edc3f2831ffd5913e486253
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-11-06 23:28:57 -0800

    Wrote decode logic

---
+++ set1/exercise6/decompose.js
@@ -0,0 +1,74 @@
+var metric = require('./metric.js');
+
+module.exports = decompose;
+
+// given a key length n and an array of data, create a Decomposed object
+function decompose(data, keyLength) {
+  var dataArray = [], i = 0, j;
+  for (i = 0; i < keyLength; ++i) dataArray.push([]);
+
+  for (i = 0, j = 0; i < data.length; ++i, ++j) {
+    if (j === keyLength)
+      j = 0;
+    dataArray[j].push(data[i]);
+  }
+
+  return new Decomposed(dataArray);
+}
+
+function Decomposed(dataArray) {
+  this.dataArray = dataArray;
+  this.key = '';
+  this.keyLength = dataArray.length;
+}
+
+Decomposed.prototype = {
+  // get the value of the key used to decode the data
+  bestKey: function () {
+    if (this.key.length === 0) {
+      this.doDecode();
+    }
+    return this.key;
+  },
+
+  // get the decoded string
+  decodedString: function () {
+    if (this.key.length === 0) {
+      this.doDecode();
+    }
+    return this.recompose();
+  },
+
+  // calculate the decoded string from the decoded data array
+  recompose: function () {
+    var string = '';
+    for (var i = 0; i < this.decoded[0].length; ++i) {
+      for (var j = 0; j < this.decoded.length; ++j) {
+        if (i === this.decoded[j].length)
+          break;
+        string += this.decoded[j][i];
+      }
+    }
+    return string;
+  },
+
+  // calculate the key and decode the data
+  doDecode: function () {
+    this.decoded = this.dataArray.map(function (array) {
+      var letter, bestScore = -10000000, bestArray;
+      for (var i = 0; i < 128; ++i) {
+        var attemptedDecode = array.map(function (datum) {
+          return i ^ datum;
+        });
+        var score = metric(attemptedDecode);
+        if (score > bestScore) {
+          bestScore = score;
+          bestArray = attemptedDecode;
+          letter = i;
+        }
+      }
+      this.key.push(letter);
+      return bestArray;
+    }.bind(this));
+  }
+};
---
+++ set1/exercise6/metric.js
@@ -0,0 +1,7 @@
+module.exports = metric;
+
+// return a score determining how likely it is that data is an array
+// containing english
+function metric(data) {
+
+}
