86a0d248d2de24c1591324def377359c962c2e8a
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-11-07 11:02:59 -0800

    Finished main script

--- run_s1e6.js
+++ run_s1e6.js
@@ -6,10 +6,24 @@ var bestKeyLength = require('set1/exercise6/bestKeyLength');
 var base64 = require('set1/exercise6/base64decode');
 
 function main(data) {
-  var keyLengths = bestKeyLength(data);
-  for (var i = 0; i < keyLengths.length; ++i) {
+  var keyLengths = bestKeyLength(data),
+      bestKey = '', bestString = '', bestScore = -10000000;
 
+  for (var i = 0; i < keyLengths.length; ++i) {
+    var decomposed = decompose(data, keyLengths[i]),
+        decodedString = decomposed.decodedString(),
+        score = metric(decodedString);
+
+    if (score > bestScore) {
+      bestScore = score;
+      bestString = decodedString;
+      bestKey = decomposed.bestKey();
+    }
   }
+
+  console.log("Decode Key: " + bestKey);
+  console.log("==================================================================\n");
+  console.log(decodedString);
 }
 
-base64.decodeFile(main);
+base64.decodeFile("./set1/exercise6/breakThis.txt", main);
